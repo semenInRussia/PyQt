bc7e81d9204c98214cd98f3722b723a997f95350
semenInRussia <51054743+semenInRussia@users.noreply.github.com>
2019-12-12 19:22:04 +0300

    Create calcmodel.py

---
+++ calcmodel.py
@@ -0,0 +1,10 @@
+ERROR_MSG = 'ERROR'
+
+def evaluateExpression(expression):
+    """Evaluate an expression."""
+    try:
+        result = str(eval(expression, {}, {}))
+    except Exception:
+        result = ERROR_MSG
+
+    return result
